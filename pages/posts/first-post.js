57c398b8c0251e18de7f44d2725b22d19cd1ec55
ikram-maulana <ikram075@ummi.ac.id>
2022-09-08 14:21:13 +0700

    Feat(Tp): Third-Party Javascript
    - Import third party library using `Script` tag from nextjs, menerima parameter `src`, `strategy` dan `onLoad`

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 const FirstPost = () => {
   return (
@@ -7,6 +8,13 @@ const FirstPost = () => {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnLoad"
+        onLoad={() => {
+          console.log(`Script loaded correctly, window.FB has been populated`);
+        }}
+      />
 
       <h1>First Post</h1>
       <h2>
