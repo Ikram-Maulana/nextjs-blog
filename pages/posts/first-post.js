487545ac512f76928b3bf7f211668eed06c16b34
ikram-maulana <ikram075@ummi.ac.id>
2022-09-08 14:31:20 +0700

    Feat(UI): Layout Component and CSS Module
    - Membuat layout component
    - Trying to make css module for `layout` component yang bernama `layout.module.css`
    - Import `style` on `layout.js` file
    - Apply `style` => `style.container`

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -20,7 +21,7 @@ const FirstPost = () => {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
