3492adbcc8d864bdea0c43e30076c3458fe620af
ikram-maulana <ikram075@ummi.ac.id>
2022-09-08 13:22:22 +0700

    Feat(Link): Link Component
    - Trying to navigate between pages using `Link` component, menerima props `href`

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const FirstPost = () => {
-  return <div>First Post</div>;
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 };
 
 export default FirstPost;
