50776bf0be944465e798fb86f99bd1771e292212
ikram-maulana <ikram075@ummi.ac.id>
2022-09-08 13:53:08 +0700

    Feat(Title): Metadata
    - Trying to modify the metadata of the page, such as the <title> HTML tag using `Head` component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const FirstPost = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
