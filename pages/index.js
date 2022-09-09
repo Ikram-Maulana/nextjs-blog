0dfdceac7495283c95b971d2be036b20779791e1
ikram-maulana <ikram075@ummi.ac.id>
2022-09-09 09:40:53 +0700

    Feat(UI): Polishing the Index Page
    - Update style to the `index` page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/dates";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../utils/posts";
@@ -35,11 +37,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
